3640c1b075a243b7ed1ec373d34c695443d8809d
Sam <159584133+Sheepizza@users.noreply.github.com>
2025-09-21 14:56:44 +0200

    Début carousel

--- content.js
+++ content.js
@@ -3,6 +3,7 @@ const carousel = document.getElementById("carousel");
 
 const AlienDatas = [
     { type: "image", src: "page.jpg" },
+    { type: "image", src: "page.jpg" },
     { type: "image", src: "page.jpg" }
 ];
 
@@ -18,3 +19,27 @@ AlienDatas.forEach(data => {
 
     carousel.appendChild(page);
 });
+
+let currentIndex = 0;
+const pages = document.querySelectorAll(".page");
+
+function updateCarousel() {
+    pages.forEach((page, i) => {
+        
+    })
+
+    const offset = -currentIndex * 320;
+    carousel.style.transform = 'translateX(${offset}px)';
+}
+
+document.getElementById("prev").addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + pages.length) % pages.length;
+    updateCarousel();
+})
+
+document.getElementById("next").addEventListener("click", () => {
+    currentIndex = (currentIndex + 1 + pages.length) % pages.length;
+    updateCarousel();
+})
+
+updateCarousel();
