daff28ff36745834ef8fd29fa18d7ad0b610cffc
Sam <smlucq@gmail.com>
2025-09-21 18:23:12 +0200

    Prod carousel

--- content.js
+++ content.js
@@ -4,6 +4,8 @@ const carousel = document.getElementById("carousel");
 const AlienDatas = [
     { type: "image", src: "page.jpg" },
     { type: "image", src: "page.jpg" },
+    { type: "image", src: "page.jpg" },
+    { type: "image", src: "page.jpg" },
     { type: "image", src: "page.jpg" }
 ];
 
@@ -24,21 +26,46 @@ let currentIndex = 0;
 const pages = document.querySelectorAll(".page");
 
 function updateCarousel() {
-    pages.forEach((page, i) => {
-        
-    })
+    const total = pages.length;
 
-    const offset = -currentIndex * 320;
-    carousel.style.transform = 'translateX(${offset}px)';
+    pages.forEach((page, i) => {
+        let offset = (i - currentIndex + total) % total;
+
+        if (offset > total / 2) {
+            offset -= total;
+        }
+
+        if (offset === 0) {
+            // carte active au centre
+            page.style.transform = "translate(-50%, 0) scale(1) translateZ(200px)";
+            page.style.opacity = "1";
+            page.style.zIndex = total;
+            page.style.filter = "brightness(1)";
+        }
+
+        else {
+            // cartes à gauche/droite
+            const translateX = offset * 220;
+            const rotateY = offset * -30;
+            const scale =  1 - 0.2 * Math.abs(offset);
+            const indexOffset = Math.abs(offset);
+            const brightnessValue = 0.7 / Math.abs(offset);
+
+            page.style.transform = `translate(-50%, 0) translateX(${translateX}px) scale(${scale}) rotateY(${rotateY}deg)`;
+            page.style.filter = `brightness(${brightnessValue})`;
+            page.style.opacity = "1";
+            page.style.zIndex = total - indexOffset;
+        }
+        });
 }
 
 document.getElementById("prev").addEventListener("click", () => {
-    currentIndex = (currentIndex - 1 + pages.length) % pages.length;
+    currentIndex = (currentIndex + 1 + pages.length) % pages.length;
     updateCarousel();
 })
 
 document.getElementById("next").addEventListener("click", () => {
-    currentIndex = (currentIndex + 1 + pages.length) % pages.length;
+    currentIndex = (currentIndex - 1 + pages.length) % pages.length;
     updateCarousel();
 })
 
