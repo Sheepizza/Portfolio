72afb0803c437d214b10141d6031853e12fa7158
Sam <159584133+Sheepizza@users.noreply.github.com>
2025-11-04 18:22:22 +0100

    Update 3D book model and enhance scene lighting
    Replaced BookTest.glb/blend with bookV0.1.glb and updated loader path. Improved lighting setup with directional and spotlights, and added GSAP camera animation for smoother transitions. Minor code refactoring and debugging output for raycaster intersections.

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 
 // --- Caméra ---
+const startCamPos = new THREE.Vector3(1.5, 5, 0);
+const endCamPos = new THREE.Vector3(0.25, 2.5, 0);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(1.5, 3, 0);
+camera.position.copy(startCamPos);
 camera.lookAt(0, 0, 0);
 
 const canvas = document.getElementById("bookCanvas");
@@ -16,9 +18,27 @@ const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 // --- Lumière ---
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(5, 5, 5);
+const light = new THREE.DirectionalLight(0xffc992, 1);
+light.position.set(0, 5, 0);
 scene.add(light);
+
+const lightR = new THREE.SpotLight(0x96b0ff, 5);
+lightR.position.set(0.6, 5, -5);
+lightR.penumbra = 0.3;
+lightR.castShadow = true;
+lightR.lookAt(0, 0, 0);
+scene.add(lightR);
+
+const lightL = new THREE.SpotLight(0xf3f8ff, 10);
+lightL.position.set(0.6, 5, 5);
+lightL.penumbra = 0.3;
+lightL.castShadow = true;
+lightL.lookAt(0, 0, 0);
+scene.add(lightL);
+
+// --- Texture ---
+//const textureLoader = new THREE.TextureLoader();
+//const texture = textureLoader.load("bois.jpg");
 
 // --- Raycaster ---
 const raycaster = new THREE.Raycaster();
@@ -33,7 +53,7 @@ const loader = new GLTFLoader();
 let mixer;
 let animations = [];
 
-loader.load('./BookTest.glb', (gltf) => {
+loader.load('./bookV0.1.glb', (gltf) => {
     const model = gltf.scene;
     scene.add(model);
 
@@ -52,37 +72,52 @@ const clock = new THREE.Clock();
 
 let lastIntersect = null;
 
+gsap.to(camera.position, {
+    x: endCamPos.x,
+    y: endCamPos.y,
+    z: endCamPos.z,
+    duration: 3,
+    ease: "power2.inOut",
+    onUpdate: () => {
+        camera.lookAt(0, 0, 0);
+    }
+});
+
 function animate() {
     requestAnimationFrame(animate);
 
+    const delta = clock.getDelta();
+
     if (mixer) {
-        const delta = clock.getDelta();
         mixer.update(delta);
     }
 
     raycaster.setFromCamera(mouse, camera);
     const intersects = raycaster.intersectObjects(scene.children, true);
 
-    /*if (intersects.length > 0) {
+    if (intersects.length > 0) {
         const obj = intersects[0].object;
-        if (obj.name === "Bookmark" && lastIntersect !== obj) {
-            const clip = THREE.AnimationClip.findByName(animations, "BookmarkSelected");
-            if (clip) {
-                const action = mixer.clipAction(clip);
-                action.setLoop(THREE.LoopOnce);
-                action.clampWhenFinished = true;
-                if (!action.isRunning())
-                    action.reset().setLoop(THREE.LoopOnce).play();
-            }
-        }
-        lastIntersect = obj;
+        console.log(obj.name);
+        //if (obj.name === "Bookmark" && lastIntersect !== obj) {
+        //    const clip = THREE.AnimationClip.findByName(animations, "BookmarkSelected");
+        //    if (clip) {
+        //        const action = mixer.clipAction(clip);
+        //        action.setLoop(THREE.LoopOnce);
+        //        action.clampWhenFinished = true;
+        //        if (!action.isRunning())
+        //            action.reset().setLoop(THREE.LoopOnce).play();
+        //    }
+        //}
+        //lastIntersect = obj;
     }
     else {
         lastIntersect = null;
-    }*/
+    }
     renderer.render(scene, camera);
 }
 animate();
+
+
 
 // --- Resize ---
 window.addEventListener('resize', () => {
